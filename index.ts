8961d7b05e490d048aa29a38fe842e4995299f62
luchoegonzalez <legonzalez@dc.uba.ar>
2025-04-16 00:53:50 -0300

    creo schema e interfaz para libro

--- index.ts
+++ index.ts
@@ -1,3 +1,22 @@
+import mongoose, {Document, Schema} from "mongoose";
 import { connectDB } from "./config/mongo";
 
-connectDB()
+connectDB() 
+
+interface LibroInterface extends Document {
+  titulo: string
+  autor: string
+  anio: number
+  genero: string
+}
+
+const libroSchema: Schema = new Schema<LibroInterface>({
+  titulo: { type: String, required: true },
+  autor: { type: String, required: true },
+  anio: { type: Number, required: true, min: 1400, max: 2025 },
+  genero: { type: String, required: true }
+}, {timestamps: false, versionKey: false})
+
+libroSchema.set("strict", true)
+
+const Libro = mongoose.model<LibroInterface>("libro", libroSchema)
